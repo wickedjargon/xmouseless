44c467a2b107aa8ce9e9a3cc6fcfbe54a45b2da3
Farzin Firouzi <farzineff@gmail.com>
2026-04-05 15:18:13 -0700

    apply custom keybindings from wickedjargon/xmouseless-old

--- config.h
+++ config.h
@@ -9,32 +9,33 @@ static const unsigned int default_speed = 500;
 
 /* changes the speed of the mouse pointer */
 static SpeedBinding speed_bindings[] = {
-    /* key             speed */
-    { XK_Super_L,      3000 },
-    { XK_Alt_L,        1500 },
-    { XK_a,            100  },
-    { XK_Control_L,    10   },
+    /* key             speed */  
+    { XK_f,      3000 },
+    { XK_d,        1500 },
+    { XK_s,            100  },
+    { XK_a,    10   },
 };
 
 /* moves the mouse pointer
  * you can also add any other direction (e.g. diagonals) */
 static MoveBinding move_bindings[] = {
     /* key         x      y */
-    { XK_j,        -1,     0 },
+    { XK_h,        -1,     0 },
     { XK_l,         1,     0 },
-    { XK_i,         0,    -1 },
-    { XK_k,         0,     1 },
+    { XK_k,         0,    -1 },
+    { XK_j,         0,     1 },
 };
 
 /* 1: left
  * 2: middle
  * 3: right */
 static ClickBinding click_bindings[] = {
-    /* key         button */
-    { XK_space,    1 },
-    { XK_f,        1 },
-    { XK_d,        2 },
-    { XK_s,        3 },
+    /* key         button */  
+    /* { XK_Space,    1 }, */
+    /* { XK_Return,     1 }, */
+    { XK_g,     1 },
+    { XK_F10,        2 },
+    { XK_space,        3 },
 };
 
 /* scrolls up, down, left and right
@@ -45,18 +46,21 @@ static ScrollBinding scroll_bindings[] = {
     { XK_p,        0 ,   -25 },
     { XK_plus,     0 ,    80 },
     { XK_minus,    0 ,   -80 },
-    { XK_h,        25,    0  },
-    { XK_g,       -25,    0  },
+    { XK_9,        25,    0  },
+    { XK_0,       -25,    0  },
 };
 
 /* executes shell commands */
 static ShellBinding shell_bindings[] = {
-    /* key         command */
-    { XK_b,        "wmctrl -a firefox" },
-    { XK_0,        "xdotool mousemove 0 0" },
+    /* key         command */  
+    /* { XK_b,        "wmctrl -a firefox" }, */
+    { XK_q,        "xdotool mousemove 2000 0" },
+
 };
 
 /* exits on key release which allows click and exit with one key */
 static KeySym exit_keys[] = {
-    XK_Escape, XK_q, XK_space
+  XK_Escape, XK_q
 };
+
+
